153a5c300bfcc53c217d1b3b9eea50abf03e216c
Antonio-Zabdiel <112569142+Antonio-Zabdiel@users.noreply.github.com>
2023-12-01 14:40:48 -0600

    final vercion

--- main.c
+++ main.c
@@ -12,55 +12,47 @@ mostrar resultados
 int randomnum();
 
 int main(){
+    srand(time(NULL));
+    
     int array1[100];
     int array2[100];
     int array3[100];
-    int i, j, k, l, m, n;//iteradores
-    int nr;//para guardar ek numero random
+    int i = 0, j = 0, k = 0, l = 0, m = 0, n = 0;//iteradores
+    int nr = 0;//para guardar ek numero random
 
     for (i = 0; i <= 99; i++){
         //printf("%d\n", i);
-        nr = randomnum();
+        //nr = randomnum();
+        nr = rand()%11;
         array1[i] = nr;  
         nr = 0;
     }
 
-    for (n = 0; n <= 99; n++)
-    {
-        printf("%d\n", array1[n]);
-    }
-    /*for (j = 0; j <= 99; j++){
-        nr = randomnum();
+    for (j = 0; j <= 99; j++){
+        //nr = randomnum();
+        nr = rand()%11;
         array2[j] = nr;   
         nr = 0; 
-        j++;
+        //j++;
     }
 
+    l = 0; m = 99;
     for (k = 0; k <= 99; k++)
     {
-        l = 0;//recorre array 1
-        m = 0;//recorre array 2
-
-        array3[k] = l*m;
+        array3[k] = array1[l]*array2[m];
 
         l++;m--;
     }
 
     for (n = 0; n <= 99; n++)
     {
-        printf("%d\t%d\t%d\t\n", array1[n], array2[n], array3[n]);
+        //printf("%d\t%d\t%d\t\n", array1[n], array2[n], array3[n]);
+        printf("%d\t%d\t%d\n", array1[n], array2[n], array3[n]);
+        //printf("%d\t\n", array2[n]);
     }
+
     //b = randomnum();
     //printf("%d\n",b);
-*/
-    return 0;
-}
 
-//pausa = getchar();
-//char pausa = ' ';
-int randomnum(){
-    int a;
-    srand(time(NULL));
-    a = rand()%21;//valor entre [0,20]
-    return a;
+    return 0;
 }
